e030d56f26602bdaa7daefa403dfbe3d75ca3672
BavSandhu <sbavneett@gmail.com>
2024-04-08 21:47:23 -0400

    updated 10 questions

--- src/QOD.jsx
+++ src/QOD.jsx
@@ -7,24 +7,104 @@ export default function () {
     const questions = [{
                 type: "radiogroup",
                 name: "question1",
-                title: "What is the most common cause of lifeguard injuries?",
+                title: "When working in a lab, what is the most appropriate choice of clothing?",
                 choices: [
-                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
+                    "Open-toe shoes", "Shorts", "Lab coat", "Bikini"
                 ],
-                correctAnswer: "Slip and fall accidents"
+                correctAnswer: "Lab coat"
             },
             {
                 type: "radiogroup",
                 name: "question2",
-                title: "How much sunscreen should you apply?",
+                title: "How often should gloves be worn in the lab when handling chemicals?",
                 choices: [
-                    "teaspoon", "tablespoon", "quart", "gallon"
+                    "Always", "Not much, its optional", "Never", "During supervision only"
                 ],
-                correctAnswer: "tablespoon"
+                correctAnswer: "Always"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question3",
+                title: "What should you do if you spill any chemical on yourself?",
+                choices: [
+                    "Immediately clean it up with the correct procedures", "Rub it with your gloves", "Wait an hour", "Ignore it and eat lunch"
+                ],
+                correctAnswer: "Immediately clean it up with the correct procedures"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question4",
+                title: "How do you deal with broken glassware?",
+                choices: [
+                    "Leave it there", "Scream at the top of your lungs", "Kick it at your lab partner", "Use a dustpan to gather the peices and throw it in a container for glass disposal"
+                ],
+                correctAnswer: "Use a dustpan to gather the peices and throw it in a container for glass disposal"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question5",
+                title: "When are food and drink allowed in the lab?",
+                choices: [
+                    "Whenever I get hungry", "Never", "When experiments aren't being conducted", "During supervision only"
+                ],
+                correctAnswer: "Never"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question6",
+                title: "What does the W in WHMIS stand for?",
+                choices: [
+                    "Work", "Worry", "Workplace", "Winning"
+                ],
+                correctAnswer: "Workplace"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question7",
+                title: "What does the H in WHMIS stand for?",
+                choices: [
+                    "Hello", "Hazardous", "Hungry", "Hazards"
+                ],
+                correctAnswer: "Hazardous"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question8",
+                title: "What does the M in WHMIS stand for?",
+                choices: [
+                    "Materials", "Money", "Manpower", "Male"
+                ],
+                correctAnswer: "Materials"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question9",
+                title: "What does the I in WHMIS stand for?",
+                choices: [
+                    "Ignite", "Iglo", "Information", "Insecure"
+                ],
+                correctAnswer: "Information"
+            },
+
+            {
+                type: "radiogroup",
+                name: "question10",
+                title: "What does the S in WHMIS stand for?",
+                choices: [
+                    "Situation", "System", "Scientist", "Safety"
+                ],
+                correctAnswer: "System"
             }];
     const nQuestion = Math.floor((Math.random() * questions.length));
     const surveyJson = {
-        title: "Lifeguard Safety",
+        title: "Lab Safety",
         showCorrectAnswer: "always",
         showProgressBar: "bottom",
         firstPageIsStarted: true,
@@ -32,7 +112,7 @@ export default function () {
         pages: [{
             elements: [{
                 type: "html",
-                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+                html: "You are about to start a quiz on Lab Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
             }, {
                 type: "text",
                 name: "username",
